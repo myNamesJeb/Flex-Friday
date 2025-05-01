af3158194c41f1fe2563884e72502b175cd02784
myNamesJeb <wilderfourbees@gmail.com>
2025-05-01 10:42:07 -0400

    fixed "clock"

--- src/printf.c
+++ src/printf.c
@@ -2,6 +2,7 @@
 #include "stdio.h"
 #include "stdarg.h"
 #include "io.h"  // we'll define an outb() in io.h (see below)
+#include "string.h"
 
 #define VIDEO_MEMORY ((volatile char*)0xb8000)
 #define SCREEN_WIDTH 80
@@ -42,52 +43,72 @@ void printf(const char *format, ...) {
     
     for (const char *p = format; *p != '\0'; p++) {
         if (*p == '%') {
-            p++;
-            if (*p == 'x') {
-                unsigned int num = va_arg(args, unsigned int);
-                char buf[9];
-                buf[8] = '\0';
-                for (int i = 7; i >= 0; --i) {
-                    int digit = num & 0xF;
-                    buf[i] = digit < 10 ? '0' + digit : 'A' + (digit - 10);
-                    num >>= 4;
-                }
-                printstr(buf);
+            p++; // move past '%'
+            int width = 0;
+            char pad = ' '; 
+            if (*p == '0') {
+                pad = '0';
+                p++;
             }
-            else if (*p == 'd') {
-                int num = va_arg(args, int);
-                char buf[12]; // enough for signed 32-bit
-                int i = 0;
-                int is_negative = 0;
-                if(num < 0) {
-                    is_negative = 1;
-                    num = -num;
-                }
-                // Convert number to string (in reverse)
-                do {
-                    buf[i++] = '0' + (num % 10);
-                    num /= 10;
-                } while(num > 0);
-                if(is_negative)
-                    buf[i++] = '-';
-                buf[i] = '\0';
-                // Reverse string in place
-                for (int j = 0; j < i/2; j++) {
-                    char temp = buf[j];
-                    buf[j] = buf[i-1-j];
-                    buf[i-1-j] = temp;
-                }
-                printstr(buf);
+            while (*p >= '0' && *p <= '9') {
+                width = width * 10 + (*p - '0');
+                p++;
             }
-            else if (*p == 's') {
-                char *str = va_arg(args, char*);
-                printstr(str);
-            }
-            else if (*p == 'c') {
-                char c = (char)va_arg(args, int);
-                putc(c);
-            } else {
-                putc(*p);
+            switch(*p) {
+                case 'x': {
+                    unsigned int num = va_arg(args, unsigned int);
+                    char buf[9];
+                    buf[8] = '\0';
+                    unsigned int temp = num;
+                    for (int i = 7; i >= 0; i--) {
+                        int digit = temp & 0xF;
+                        buf[i] = (digit < 10) ? '0' + digit : 'A' + (digit - 10);
+                        temp >>= 4;
+                    }
+                    int len = strlen(buf);
+                    int num_pad = (width > len) ? width - len : 0;
+                    for (int i = 0; i < num_pad; i++)
+                        putc(pad);
+                    printstr(buf);
+                    break;
+                }
+                case 'd': {
+                    int num = va_arg(args, int);
+                    char buf[12];
+                    int i = 0;
+                    int is_negative = 0;
+                    if(num < 0) {
+                        is_negative = 1;
+                        num = -num;
+                    }
+                    do {
+                        buf[i++] = '0' + (num % 10);
+                        num /= 10;
+                    } while(num > 0);
+                    if(is_negative)
+                        buf[i++] = '-';
+                    buf[i] = '\0';
+                    for (int j = 0; j < i/2; j++) {
+                        char tmp = buf[j];
+                        buf[j] = buf[i - 1 - j];
+                        buf[i - 1 - j] = tmp;
+                    }
+                    int len = strlen(buf);
+                    int num_pad = (width > len) ? width - len : 0;
+                    for (int i = 0; i < num_pad; i++)
+                        putc(pad);
+                    printstr(buf);
+                    break;
+                }
+                case 's':
+                    printstr(va_arg(args, char *));
+                    break;
+                case 'c':
+                    putc((char)va_arg(args, int));
+                    break;
+                default:
+                    putc(*p);
+                    break;
             }
         } else {
             putc(*p);
